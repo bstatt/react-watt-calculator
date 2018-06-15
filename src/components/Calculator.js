fd870ea511a64e15563a5a8734c390180a0c21fb
Britany Statt <bstatt2@gmail.com>
2018-06-15 14:06:59 -0700

    changed to form tag, started event handlers

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,7 +4,29 @@ import inverterData from './../data/inverters';
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = { inverters: inverterData };
+    this.state = {
+      inverters: inverterData,
+      amps: '',
+      volts: '',
+      item: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log("submit!");
+    event.preventDefault();
   }
 
   render() {
@@ -13,12 +35,13 @@ class Calculator extends Component {
         <h1>Which Inverter Do I Need?</h1>
         <p>Please add the following information about the items you need to power:</p>
         <div className="input-inverter-info">
-          <ul>
-            <li>Item:<input type="text" id="item" value="" className="inputs"/></li>
-            <li>Amps:<input type="text" id="amps" value="" className="inputs"/></li>
-            <li>Volts:<input type="text" id="volts" value="" className="inputs"/></li>
-          </ul>
-          <button id="add-button" type="button">Add Appliance</button>
+          <form onSubmit={this.handleSubmit}>
+            Item:<input type="text" id="item" value={this.state.item} onChange={this.handleInputChange} className="inputs"/>
+            Amps:<input type="text" id="amps" value={this.state.amps} onChange={this.handleInputChange} className="inputs"/>
+            Volts:<input type="text" id="volts" value={this.state.volts} onChange={this.handleInputChange} className="inputs"/>
+            <input type="submit" value="Add Appliance" />
+          </form>
+
           <table id="appliance-table">
             <tbody id="tbody">
               <tr>
@@ -26,6 +49,9 @@ class Calculator extends Component {
                 <th>Amps</th>
                 <th>Volts</th>
                 <th>Watts</th>
+              </tr>
+              <tr>
+                <td>{this.state.value}</td>
               </tr>
             </tbody>
           </table>
